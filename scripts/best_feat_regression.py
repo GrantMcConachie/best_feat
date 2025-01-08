a97c4ff583654ecc11418c701b69ca880a2aee18
Grant Mcconachie <grantmcconachie@crc-dot1x-nat-10-239-73-25.bu.edu>
2025-01-08 15:53:45 -0500

    made a shuffle and a scaffold split, and some changes to how output is presented

--- scripts/best_feat_regression.py
+++ scripts/best_feat_regression.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
 from sklearn.linear_model import Ridge, Lasso
-from sklearn.model_selection import GridSearchCV, PredefinedSplit, ShuffleSplit
+from sklearn.model_selection import GridSearchCV, ShuffleSplit, KFold
 
 from molfeat.trans import MoleculeTransformer
 from molfeat.trans.fp import FPVecTransformer
@@ -142,15 +142,18 @@ def run_lr(dataset, embs, regressor='r'):
     # init
     seed = 12345
     scaffoldsplitter = dc.splits.ScaffoldSplitter()
+    ss = KFold(n_splits=5, shuffle=False)
     rs = ShuffleSplit(n_splits=5, test_size=0.2, random_state=seed)
     df = pd.read_csv(dataset)
-    proteins = df['Protein'].unique()
+    proteins = df['Protein sequence'].unique()
 
     # loop through unique proteins
     scores_per_protein = []
     for protein in proteins:
-        smiles = df['SMILES'][df['Protein'] == protein]
-        output = zscore(np.array(df['output'][df['Protein'] == protein]))
+        smiles = df['SMILES'][df['Protein sequence'] == protein]
+        output = zscore(
+            np.array(df['output'][df['Protein sequence'] == protein])
+        )
         embeddings = np.array([embs[i] for i in smiles])
 
         # defining regressor
@@ -177,52 +180,46 @@ def run_lr(dataset, embs, regressor='r'):
             r2 = best_model.score(embeddings[test_index], output[test_index])
             random_shuf_scores.append(r2)
 
-        # scaffold split molecules
+        # 5-fold cross val for scaffold splitting
         dataset_ = dc.data.NumpyDataset(
             X=embeddings,
             y=output,
             ids=smiles
         )
-        train, val, test = scaffoldsplitter.train_valid_test_split(
-            dataset_,
-            seed=seed
-        )
+        scaf_out = np.array([
+            output[x]
+            for xs in scaffoldsplitter.generate_scaffolds(dataset_)
+            for x in xs
+        ])
+        scaf_emb = np.array([
+            embeddings[x]
+            for xs in scaffoldsplitter.generate_scaffolds(dataset_)
+            for x in xs
+        ])
+        scaf_shuf_scores = []
+        for i, (train_index, test_index) in enumerate(ss.split(scaf_out)):
+            clf = GridSearchCV(
+                reg,
+                param_grid=param_grid,
+            )
+            clf.fit(scaf_emb[train_index], scaf_out[train_index])
+            best_model = clf.best_estimator_
+            best_model.fit(scaf_emb[train_index], scaf_out[train_index])
+            r2 = best_model.score(scaf_emb[test_index], scaf_out[test_index])
+            scaf_shuf_scores.append(r2)
 
-        # specify train and val for hyperparameter sweep
-        train_val_x = np.concatenate([train.X, val.X])
-        train_val_y = np.concatenate([train.y, val.y])
-        test_fold = np.concatenate(
-            [
-                -1 * np.ones(shape=len(train.y)),
-                np.zeros(shape=len(val.y))
-            ]
-        )
-
-        # hyperparameter search
-        clf = GridSearchCV(
-            reg,
-            param_grid=param_grid,
-            cv=PredefinedSplit(test_fold)
-        )
-        clf.fit(train_val_x, train_val_y)
-
-        # evaluating metrics
-        clf.best_estimator_.fit(X=train_val_x, y=train_val_y)
-
-        # predicting test set and recording
-        scaf_score = clf.best_estimator_.score(test.X, test.y)
         scores_per_protein.append(
             (
                 protein,
                 random_shuf_scores,
-                scaf_score
+                scaf_shuf_scores
             )
         )
 
     return scores_per_protein
 
 
-def plot_results(results, dataset, ylabel='r2'):
+def plot_results(results, dataset, regressor, ylabel='r2'):
     """
     plots embedding scores on a given dataset
     """
@@ -234,9 +231,10 @@ def plot_results(results, dataset, ylabel='r2'):
     emb_scores_P_shuf = []
     emb_scores_P_scaf = []
 
+    print(dataset, f'regressor: {regressor}')
     for result in results:
         shuf_protein_scores = np.array([i[1] for i in result[1]]).flatten()
-        scaf_protein_scores = np.array([i[2] for i in result[1]])
+        scaf_protein_scores = np.array([i[2] for i in result[1]]).flatten()
         if result[0][0] == 'graph' or result[0][0] == 'trans':
             emb_names_NN.append(result[0][1])
             emb_scores_NN_shuf.append(shuf_protein_scores)
@@ -247,49 +245,122 @@ def plot_results(results, dataset, ylabel='r2'):
             emb_scores_P_shuf.append(shuf_protein_scores)
             emb_scores_P_scaf.append(scaf_protein_scores)
 
-    # plotting
-    fig, axs = plt.subplots(1, 2)
+        # ouput to terminal
+        print(result[0][1])
+        print('shuf mean:', np.mean(shuf_protein_scores))
+        print('shuf std:', np.std(shuf_protein_scores))
+        print('scaf mean:', np.mean(scaf_protein_scores))
+        print('scaf std:', np.std(scaf_protein_scores), '\n')
 
-    # physicochemical descriptors
-    axs[0].violinplot(
-        np.array(emb_scores_P_shuf).T,
-        showmeans=True,
-        positions=np.arange(len(emb_names_P))
-        )
-    for i, row in enumerate(emb_scores_P_shuf):
-        axs[0].scatter(
-            np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
-        )
-    # axs[0].violinplot(emb_scores_P_scaf)
-    axs[0].set_title('Physicochemical Descriptors')
-    axs[0].set_ylabel(ylabel)
-    axs[0].set_ylim([-3, 1])
-    axs[0].set_xticks(range(len(emb_names_P)))
-    axs[0].set_xticklabels(emb_names_P)
+    # plot for scaffold and random shuffled
+    for i, (nn, p) in enumerate([
+        (emb_scores_NN_scaf, emb_scores_P_scaf),
+        (emb_scores_NN_shuf, emb_scores_P_shuf)
+    ]):
+        fig, axs = plt.subplots(1, 2)
 
-    # Neural network embeddings
-    axs[1].violinplot(
-        np.array(emb_scores_NN_shuf).T,
-        showmeans=True,
-        positions=np.arange(len(emb_names_NN))
+        # physicochemical descriptors
+        axs[0].violinplot(
+            np.array(p).T,
+            showmeans=True,
+            positions=np.arange(len(emb_names_P))
+            )
+        for i, row in enumerate(p):
+            axs[0].scatter(
+                np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
+            )
+        axs[0].set_title('Physicochemical Descriptors')
+        axs[0].set_ylabel(ylabel)
+        axs[0].set_ylim([-3, 1])
+        axs[0].set_xticks(range(len(emb_names_P)))
+        axs[0].set_xticklabels(emb_names_P)
+
+        # Neural network embeddings
+        axs[1].violinplot(
+            np.array(nn).T,
+            showmeans=True,
+            positions=np.arange(len(emb_names_NN))
+        )
+        for i, row in enumerate(nn):
+            axs[1].scatter(
+                np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
+            )
+        axs[1].set_title('Neural Network Embeddings')
+        axs[1].set_ylabel(ylabel)
+        axs[1].set_ylim([-3, 1])
+        axs[1].set_xticks(range(len(emb_names_NN)))
+        axs[1].set_xticklabels(emb_names_NN)
+
+        if i == 0:
+            title = " scaffold split"
+        else:
+            title = " shuffle split"
+
+        fig.suptitle(dataset + title)
+        fig.autofmt_xdate(rotation=45)
+        plt.tight_layout()
+
+
+def tabulate(emb_scores, dataset, regressor):
+    """
+    Saves data to a csv
+    """
+    # create empty dataframe and a place for it to go
+    path = os.path.dirname(dataset).replace("data/", "results/")
+    path += "/molecule_emb_only.csv"
+    df = pd.DataFrame(
+        columns=[
+            'embedding',
+            'shuf_mean',
+            'scaf_mean',
+            'shuf1',
+            'shuf2',
+            'shuf3',
+            'shuf4',
+            'shuf5',
+            'scaf1',
+            'scaf2',
+            'scaf3',
+            'scaf4',
+            'scaf5',
+            'receptor',
+            'regressor'
+        ]
     )
-    for i, row in enumerate(emb_scores_NN_shuf):
-        axs[1].scatter(
-            np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
-        )
-    # axs[1].violinplot(emb_scores_NN_scaf)
-    axs[1].set_title('Neural Network Embeddings')
-    axs[1].set_ylabel(ylabel)
-    axs[1].set_ylim([-3, 1])
-    axs[1].set_xticks(range(len(emb_names_NN)))
-    axs[1].set_xticklabels(emb_names_NN)
 
-    fig.suptitle(dataset)
-    fig.autofmt_xdate(rotation=45)
-    plt.tight_layout()
+    # loop through scores
+    for score in emb_scores:
+        emb = score[0][1]
+        for prot in score[1]:
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame({
+                        'embedding': [emb],
+                        'shuf_mean': [np.mean(prot[1])],
+                        'scaf_mean': [np.mean(prot[2])],
+                        'shuf1': [prot[1][0]],
+                        'shuf2': [prot[1][1]],
+                        'shuf3': [prot[1][2]],
+                        'shuf4': [prot[1][3]],
+                        'shuf5': [prot[1][4]],
+                        'scaf1': [prot[2][0]],
+                        'scaf2': [prot[2][1]],
+                        'scaf3': [prot[2][2]],
+                        'scaf4': [prot[2][3]],
+                        'scaf5': [prot[2][4]],
+                        'receptor': [prot[0]],
+                        'regressor': regressor
+                    })
+                ],
+                ignore_index=True
+            )
+
+    # save
+    df.to_csv(path, index=False)
 
 
-def main(datasets):
+def main(datasets, regressor='r'):
     emb_types = get_embedding_types()
     for dataset in datasets:
         emb_scores = []
@@ -297,18 +368,18 @@ def main(datasets):
             embs = generate_embeddings(dataset, emb_type)
             embs = clean_embeddings(embs)
             embs = reduce_embs(embs)
-            scores = run_lr(dataset, embs, regressor='r')
-
+            scores = run_lr(dataset, embs, regressor=regressor)
             emb_scores.append((emb_type, scores))
 
-        plot_results(emb_scores, dataset)
+        plot_results(emb_scores, dataset, regressor)
+        tabulate(emb_scores, dataset, regressor)
 
     plt.show()
 
 
 if __name__ == '__main__':
     datasets = [
-        'data/HallemCarlson/hc_data_reformat.csv',
-        'data/Davis/davis.csv'
+        # 'data/Davis/davis.csv',
+        'data/HallemCarlson/hc_with_prot_seq.csv'
     ]
     main(datasets)
