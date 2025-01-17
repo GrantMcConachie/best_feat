2b705598e0dae881db3ab0015a81071a42104867
Grant Mcconachie <grantmcconachie@crc-dot1x-nat-10-239-73-25.bu.edu>
2025-01-17 16:39:59 -0500

    small changes

--- utils/analyze_davis.py
+++ utils/analyze_davis.py
@@ -9,8 +9,19 @@ import pandas as pd
 df = pd.read_csv("results/Davis/molecule_emb_only_w_prob_receptors.csv")
 
 # indicies with receptors with over 10 r2 value
-big_r2_idx = df.index[np.abs(df['shuf_mean']) > 10].to_list()
-prob_receptors = list(set(list(df['receptor'][big_r2_idx])))
+big_r2_idx = df.index[
+    (np.abs(df['shuf_mean']) > 10) | (np.abs(df['scaf_mean']) > 10)
+].to_list()
+prob_receptors = list(set(df['receptor'][big_r2_idx]))
 
+# removing annoying receptors
+for index, row in df.iterrows():
+    if row['receptor'] in prob_receptors:
+        df.drop(index, inplace=True)
 
-print(len(prob_receptors))
+# loop through embeddings
+for emb in df['embedding'].unique():
+    print(emb)
+    print('shuf_mean:', df[df['embedding'] == emb]['shuf_mean'].mean())
+    print('scaf_mean:', df[df['embedding'] == emb]['scaf_mean'].mean())
+    print('\n')
