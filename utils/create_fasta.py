bf3d29caada605fff612c95af77736f08cb90b66
Grant Mcconachie <grantmcconachie@crc-dot1x-nat-10-239-73-25.bu.edu>
2025-01-17 16:39:11 -0500

    script for creating fasta files out of the csvs

---
+++ utils/create_fasta.py
@@ -0,0 +1,23 @@
+"""
+Creates fasta files for the CDhit algorithm
+"""
+
+import os
+import pandas as pd
+
+
+def main(dataset):
+    # read csv
+    df = pd.read_csv(dataset)
+    prots = list(df['Protein sequence'].unique())
+
+    # make fasta file
+    save_path = os.path.dirname(dataset) + "/cdhit/cd.fasta"
+    with open(save_path, 'w') as f:
+        for i, row in enumerate(prots):
+            f.write(f'>seq{i}\n{row}\n')
+
+
+if __name__ == '__main__':
+    dataset = 'data/Davis/davis.csv'
+    main(dataset)
