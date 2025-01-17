2e469ec4daa28004961d5207f237e500c60e5e18
Grant Mcconachie <grantmcconachie@crc-dot1x-nat-10-239-73-25.bu.edu>
2025-01-17 16:40:47 -0500

    code for testing cdhit split

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ Combination model of molecules and proteins for prediction. A linear regression
 with a protein embeddings concatenated to a molecule embedding.
 
 TODO:
- - CDhit proteins / scffold split molecules
+ - scffold split molecules
 """
 
 import os
@@ -14,7 +14,7 @@ from tqdm import tqdm
 from scipy.stats import zscore
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge, Lasso
-from sklearn.model_selection import ShuffleSplit, GridSearchCV
+from sklearn.model_selection import ShuffleSplit, GridSearchCV, KFold
 
 import esm
 import torch
@@ -55,12 +55,12 @@ def generate_embeddings(dataset, mol_emb_type):
     # chunking into size 10 batches for cpu
     split_batch_tokens = torch.tensor_split(
         batch_tokens,
-        len(batch_tokens) // 10,
+        len(batch_tokens) // 2,
         dim=0
     )
     split_batch_lens = torch.tensor_split(
         batch_lens,
-        len(batch_tokens) // 10,
+        len(batch_tokens) // 2,
     )
 
     # Extract per-residue representations
@@ -90,6 +90,42 @@ def generate_embeddings(dataset, mol_emb_type):
     pkl.dump(feat_dict, open(save_path, "wb"))
 
     return (mol_embs, feat_dict)
+
+
+def cdhit_split(df, x, y, dataset):
+    """
+    splits data based on protein similarity
+    """
+    # paths of results from cdhit
+    cluster_path = os.path.dirname(dataset) + '/cdhit/cd_out.clstr'
+    prot_path = os.path.dirname(dataset) + '/cdhit/cd.fasta'
+
+    # get all proteins and their labels
+    seq = {}
+    with open(prot_path, 'r') as f:
+        for line in f:
+            seq_id = line.split('>')[1].split('\n')[0]
+            seq[seq_id] = next(f).split('\n')[0]
+
+    # order proteins associated with their cluster
+    ordered_prots = []
+    with open(cluster_path, 'r') as f:
+        for line in f:
+            if line.startswith('>Cluster'):
+                pass
+            else:
+                seq_id = line.split('>')[1].split('...')[0]
+                ordered_prots.append(seq[seq_id])
+
+    # put the indicies of the data in an order according to the clusters
+    split_list = []
+    for p in ordered_prots:
+        split_list += list(df.index[df['Protein sequence'] == p])
+
+    x_reorder = x[split_list]
+    y_reorder = y[split_list]
+
+    return x_reorder, y_reorder
 
 
 def run_regression(dataset, mol_emb, prot_emb, regressor='r'):
@@ -125,11 +161,9 @@ def run_regression(dataset, mol_emb, prot_emb, regressor='r'):
         'alpha': np.logspace(-10, 10, num=21)
     }
 
-    # defining splits of the data
+    # random splits of the data
     rs = ShuffleSplit(n_splits=5, test_size=0.2, random_state=seed)
-
-    # performing cross val
-    scores = []
+    rand_shuf_scores = []
     for train_index, test_index in rs.split(y):
         clf = GridSearchCV(
             reg,
@@ -139,67 +173,96 @@ def run_regression(dataset, mol_emb, prot_emb, regressor='r'):
         best_model = clf.best_estimator_
         best_model.fit(x[train_index], y[train_index])
         r2 = best_model.score(x[test_index], y[test_index])
-        scores.append(r2)
+        rand_shuf_scores.append(r2)
 
-    return scores
+    # cdhit splits of the data
+    x, y = cdhit_split(df, x, y, dataset=dataset)
+    cdhit_scores = []
+    for train_index, test_index in KFold(n_splits=5, shuffle=False).split(y):
+        clf = GridSearchCV(
+            reg,
+            param_grid=param_grid,
+        )
+        clf.fit(x[train_index], y[train_index])
+        best_model = clf.best_estimator_
+        best_model.fit(x[train_index], y[train_index])
+        r2 = best_model.score(x[test_index], y[test_index])
+        cdhit_scores.append(r2)
+
+    return (rand_shuf_scores, cdhit_scores)
 
 
 def plot_results(emb_scores, dataset, ylabel='r2'):
-    scores_NN = []
-    scores_P = []
+    shuf_scores_NN = []
+    shuf_scores_P = []
+    cdhit_scores_NN = []
+    cdhit_scores_P = []
     emb_names_NN = []
     emb_names_P = []
 
     for i in emb_scores:
         if i[0][0] == 'graph' or i[0][0] == 'trans':
-            scores_NN.append(i[1])
+            shuf_scores_NN.append(i[1])
+            cdhit_scores_NN.append(i[2])
             emb_names_NN.append(i[0][1])
-            print(f'{i[0][1]}: mean {np.mean(i[1])} std: {np.std(i[1])}')
+            print(f'{i[0][1]} shuf: mean {np.mean(i[1])} std: {np.std(i[1])}')
+            print(f'{i[0][1]} cdhit: mean {np.mean(i[2])} std: {np.std(i[2])}')
 
         else:
-            scores_P.append(i[1])
+            shuf_scores_P.append(i[1])
+            cdhit_scores_P.append(i[2])
             emb_names_P.append(i[0][1])
-            print(f'{i[0][1]}: mean {np.mean(i[1])} std: {np.std(i[1])}')
+            print(f'{i[0][1]} shuf: mean {np.mean(i[1])} std: {np.std(i[1])}')
+            print(f'{i[0][1]} cdhit: mean {np.mean(i[2])} std: {np.std(i[2])}')
 
     # plotting
-    fig, axs = plt.subplots(1, 2)
+    for j, (nn, p) in enumerate([
+        (shuf_scores_NN, shuf_scores_P),
+        (cdhit_scores_NN, cdhit_scores_P)
+    ]):
+        fig, axs = plt.subplots(1, 2)
 
-    # physicochemical descriptors
-    axs[0].violinplot(
-        np.array(scores_P).T,
-        showmeans=True,
-        positions=np.arange(len(emb_names_P))
-        )
-    for i, row in enumerate(scores_P):
-        axs[0].scatter(
-            np.ones_like(row) * i, row, color="#cc6666", alpha=0.7, s=5
-        )
-    axs[0].set_title('Physicochemical Descriptors')
-    axs[0].set_ylabel(ylabel)
-    axs[0].set_ylim([-3, 1])
-    axs[0].set_xticks(range(len(emb_names_P)))
-    axs[0].set_xticklabels(emb_names_P)
+        # physicochemical descriptors
+        axs[0].violinplot(
+            np.array(p).T,
+            showmeans=True,
+            positions=np.arange(len(emb_names_P))
+            )
+        for i, row in enumerate(p):
+            axs[0].scatter(
+                np.ones_like(row) * i, row, color="#cc6666", alpha=0.7, s=5
+            )
+        axs[0].set_title('Physicochemical Descriptors')
+        axs[0].set_ylabel(ylabel)
+        axs[0].set_ylim([-3, 1])
+        axs[0].set_xticks(range(len(emb_names_P)))
+        axs[0].set_xticklabels(emb_names_P)
 
-    # Neural network embeddings
-    axs[1].violinplot(
-        np.array(scores_NN).T,
-        showmeans=True,
-        positions=np.arange(len(emb_names_NN))
-    )
-    for i, row in enumerate(scores_NN):
-        axs[1].scatter(
-            np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
+        # Neural network embeddings
+        axs[1].violinplot(
+            np.array(nn).T,
+            showmeans=True,
+            positions=np.arange(len(emb_names_NN))
         )
-    # axs[1].violinplot(emb_scores_NN_scaf)
-    axs[1].set_title('Neural Network Embeddings')
-    axs[1].set_ylabel(ylabel)
-    axs[1].set_ylim([-3, 1])
-    axs[1].set_xticks(range(len(emb_names_NN)))
-    axs[1].set_xticklabels(emb_names_NN)
+        for i, row in enumerate(nn):
+            axs[1].scatter(
+                np.ones_like(row) * i, row, color="#cc6666", alpha=0.3, s=5
+            )
+        # axs[1].violinplot(emb_scores_NN_scaf)
+        axs[1].set_title('Neural Network Embeddings')
+        axs[1].set_ylabel(ylabel)
+        axs[1].set_ylim([-3, 1])
+        axs[1].set_xticks(range(len(emb_names_NN)))
+        axs[1].set_xticklabels(emb_names_NN)
 
-    fig.suptitle(dataset)
-    fig.autofmt_xdate(rotation=45)
-    plt.tight_layout()
+        if j == 0:
+            title = ' shuffle'
+        else:
+            title = ' cdhit'
+
+        fig.suptitle(dataset + title)
+        fig.autofmt_xdate(rotation=45)
+        plt.tight_layout()
 
 
 def main(dataset):
@@ -210,15 +273,20 @@ def main(dataset):
             mol_emb, prot_emb = generate_embeddings(dataset, emb_type)
             mol_emb = r.clean_embeddings(mol_emb)
             mol_emb = r.reduce_embs(mol_emb)  # idk about this
-            scores = run_regression(dataset, mol_emb, prot_emb, regressor='r')
-            emb_scores.append((emb_type, scores))
+            shuf_scores, cdhit_scores = run_regression(
+                dataset,
+                mol_emb,
+                prot_emb,
+                regressor='r'
+            )
+            emb_scores.append((emb_type, shuf_scores, cdhit_scores))
         plot_results(emb_scores, dataset)
     plt.show()
 
 
 if __name__ == '__main__':
     datasets = [
-        # 'data/Davis/davis.csv',
-        'data/HallemCarlson/hc_with_prot_seq_z.csv',
+        'data/Davis/davis.csv',
+        # 'data/HallemCarlson/hc_with_prot_seq.csv',
     ]
     main(datasets)
